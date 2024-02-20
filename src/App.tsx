dbe243436e866a623f039394e7841c2668cc1d9a
PavelBerikov <NoooB199505@gmail.com>
2024-02-20 22:55:45 +0200

    master

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
 import React from 'react';
-import {Navigate, Route, Routes} from "react-router-dom";
-import {MainLayout} from "./layouts/MainLayout";
-import {UsersPage} from "./pages/UsersPage";
-import {UserDetailsPage} from "./pages/UserDetailsPage";
+
 
 const App = () => {
     return (
-        <Routes>
-            <Route path={'/'} element={<MainLayout/>}>
-                <Route index element={<Navigate to={'users'}/>}/>
-                <Route path={'users'} element={<UsersPage/>}>
-                    <Route path={':id'} element={<UserDetailsPage/>}/>
-                </Route>
-            </Route>
-
-        </Routes>
+        <div>
+            App
+        </div>
     );
 };
 
